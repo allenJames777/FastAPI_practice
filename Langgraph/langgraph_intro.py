b8dca11ac36b5ff116947bd903ecc532b6b3af52
Allen James Lluvido <jazzwaters777@gmail.com>
2026-04-10 00:26:37 +0800

    practice langgraph

---
+++ Langgraph/langgraph_intro.py
@@ -0,0 +1,29 @@
+import os
+os.environ["GROQ_API_KEY"] = "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"
+
+from langchain_groq import ChatGroq
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict
+
+class State(TypedDict):
+    message: str
+    response: str
+
+def chat(state: State):
+    llm = ChatGroq(model="llama-3.1-8b-instant")
+    result = llm.invoke(state["message"])
+    return {"response": result.content}
+
+graph = StateGraph(State)
+graph.add_node("chat", chat)
+graph.add_edge(START, "chat")
+graph.add_edge("chat", END)
+
+app = graph.compile()
+
+while True:
+    user_input = input("You: ")
+    if user_input == "quit":
+        break
+    result = app.invoke({"message": user_input})
+    print(f"AI: {result['response']}")
